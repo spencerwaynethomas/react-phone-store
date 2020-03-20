727563178dd1ed742acaf2310ba986d66ca2ee85
Spencer Thomas <spencerthomas@Spencers-MacBook-Air.local>
2020-03-20 17:15:35 -0600

    data and images

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import  'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  render() {
+    return (
+    <div className="container">
+      <div classname="row">
+        <div className="col-6">column Number one</div>
+        <div className="col-6">column Number one</div>
+      </div>
     </div>
-  );
+    )
+  }
 }
-
 export default App;
